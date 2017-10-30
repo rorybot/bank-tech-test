b537ae655a795e74c3691928199f43a51f937e67
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-30 15:39:08 +0000

    RED: transaction history stores balance at time of input

--- spec/deposits_spec.rb
+++ spec/deposits_spec.rb
@@ -7,12 +7,13 @@ describe Account do
       expect(test_account.query_balance(test_account.transaction_history)).to eq 2000
     end
 
-    it 'can store the amount and date of deposit' do
+    it 'can store the amount and date and current balance of deposit' do
       2.times { test_account.change_balance('deposit', 1000, '10-01-2012') }
       p test_account
       expect(test_account.transaction_history.first[:amount]).to eq 1000
       expect(test_account.transaction_history.first[:date].to_i).to eq 1326153600
       expect(test_account.transaction_history.first[:date].year).to eq 2012
+      expect(test_account.transaction_history.first[:current_balance]).to eq 2000
     end
 
     it 'can make withdrawls from balance' do

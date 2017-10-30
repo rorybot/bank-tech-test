8f7723d0b6b3fbe7b894366e452c129abb8444e0
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-30 12:07:16 +0000

    RED: Store date with deposit

--- spec/deposits_spec.rb
+++ spec/deposits_spec.rb
@@ -9,10 +9,15 @@ describe Account do
 
   context 'when interacting with an account' do
     it 'can store a record of deposits' do
-      empty_account.deposit 1000
-      empty_account.deposit 2000
-      expect(empty_account.balance).to eq 3000
+      2.times {empty_account.deposit 1000}
+      expect(empty_account.balance).to eq 2000
       expect(empty_account.deposits_history.first).to eq 1000
+    end
+
+    it 'can store the date of deposit' do
+      empty_account.deposit 1000, "10-01-2012"
+      expect(empty_account.deposits_history.first.amount).to eq 1000
+      expect(empty_account.deposits_history.first.amount).to eq "10-01-2012"
     end
   end
 end

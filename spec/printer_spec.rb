0294777107800b8a2fce36bbccfdb507109e4528
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-30 14:31:29 +0000

    RED: Printing a transaction properly

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -4,7 +4,10 @@ describe Printer do
   context 'when accessed' do
     it 'can print a properly formatted header' do
       expect{ test_printer.print_header }.to output("date || credit || debit || balance\n").to_stdout
+    end
 
+    it 'can print a transaction' do
+      expect{ test_printer.print_transaction }.to output("10-01-2012 || 1000.00 || || 1000.00 balance\n").to_stdout
     end
   end
 

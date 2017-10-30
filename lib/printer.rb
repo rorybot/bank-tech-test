0e0d1b49ec77be51788881bd98e525ec1088ba30
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-30 15:55:26 +0000

    Reformatted printer to accept new transaction amount storage method

--- lib/printer.rb
+++ lib/printer.rb
@@ -2,15 +2,20 @@ require_relative 'account'
 
 class Printer
   def print_header
-    puts "date || credit || debit || balance"
+    puts 'date || credit || debit || balance'
   end
 
   def print_transaction(account, index, calculator = BalanceCalculator.new)
     transaction = account.transaction_history[index]
-    credit = '%.2f' % transaction[:amount] if transaction[:type] == 'deposit'
-    debit = '%.2f' % transaction[:amount] if transaction[:type] == 'withdrawl'
-    date = transaction[:date].strftime("%d/%m/%Y")
+
+    if transaction[:amount] > 0
+      credit = format('%.2f', transaction[:amount])
+    else
+      debit = format('%.2f', transaction[:amount])
+    end
+
+    date = transaction[:date].strftime('%d/%m/%Y')
     balance = calculator.calculate_balance(account.transaction_history)
-    puts string = "#{date} || #{credit} || #{debit} || #{'%.2f' % balance}"
+    puts string = "#{date} || #{credit} || #{debit} || #{format('%.2f', balance)}"
   end
 end

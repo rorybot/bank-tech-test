712a1d3b6109a106fc32aae8f4da3fdd423f44b3
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-30 12:52:07 +0000

    RED: can calcualte balance on fly; uses private sum methods

--- lib/account.rb
+++ lib/account.rb
@@ -11,7 +11,15 @@ class Account
     store_change(type, amount, date)
   end
 
-  private
+  def calculate_balance
+    the_balance = 0
+    the_balance += sum_credits(transaction_history)
+    the_balance -= sum_debits(transaction_history)
+
+    the_balance
+  end
+
+  # private
 
   def store_change(type, amount, date)
     record = { type: type, amount: amount, date: date }
@@ -20,5 +28,17 @@ class Account
 
   def transaction(type, amount)
     type == 'deposit' ? @balance += amount : @balance -= amount
+  end
+
+  def sum_credits(transaction_history)
+    credit_sum = 0
+    transaction_history.each { |each_transaction| credit_sum += each_transaction[:amount] if each_transaction[:type] == 'deposit' }
+    credit_sum
+  end
+
+  def sum_debits(transaction_history)
+    debit_sum = 0
+    transaction_history.each { |each_transaction| debit_sum += each_transaction[:amount] if each_transaction[:type] == 'withdraw' }
+    debit_sum
   end
 end

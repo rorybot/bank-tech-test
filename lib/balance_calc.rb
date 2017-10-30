67d42264b7fbdfffd6b555502f5a4293825ed25f
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-30 15:48:57 +0000

    Adjusted calculator to accept new transaction amount submission format

--- lib/balance_calc.rb
+++ lib/balance_calc.rb
@@ -1,23 +1,13 @@
 class BalanceCalculator
   def calculate_balance(transaction_history)
-    the_balance = 0
-    the_balance += sum_credits(transaction_history)
-    the_balance -= sum_debits(transaction_history)
-
-    the_balance
+    return the_balance = sum_transactions(transaction_history)
   end
 
   private
 
-  def sum_credits(transaction_history)
-    credit_sum = 0
-    transaction_history.each { |each_transaction| credit_sum += each_transaction[:amount] if each_transaction[:type] == 'deposit' }
-    credit_sum
+  def sum_transactions(transaction_history)
+    money_array = transaction_history.map{|transaction| transaction[:amount]}
+    money_array.inject(0){|sum,x| sum + x}
   end
 
-  def sum_debits(transaction_history)
-    debit_sum = 0
-    transaction_history.each { |each_transaction| debit_sum += each_transaction[:amount] if each_transaction[:type] == 'withdraw' }
-    debit_sum
-  end
 end

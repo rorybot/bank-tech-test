982f801541d4668cc1799040368145cb0d70de5e
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-30 15:31:29 +0000

    GREEN: Printing a transaction properly

--- lib/printer.rb
+++ lib/printer.rb
@@ -4,4 +4,13 @@ class Printer
   def print_header
     puts "date || credit || debit || balance"
   end
+
+  def print_transaction(account, index, calculator = BalanceCalculator.new)
+    transaction = account.transaction_history[index]
+    credit = '%.2f' % transaction[:amount] if transaction[:type] == 'deposit'
+    debit = '%.2f' % transaction[:amount] if transaction[:type] == 'withdrawl'
+    date = transaction[:date].strftime("%d/%m/%Y")
+    balance = calculator.calculate_balance
+    puts string = "#{date} || #{credit} || #{debit} || #{'%.2f' % balance}"
+  end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,13 +1,25 @@
 describe Printer do
   let(:test_printer) { Printer.new }
 
+  let(:test_account) do
+    dummy_transaction = [{ type: 'deposit', amount: 1000, date: Time.new(2012, 01, 10) }, { type: 'deposit', amount: 2000, date: Time.new(2012, 01, 13) }, { type: 'withdraw', amount: 500, date: Time.new(2012, 01, 14) }]
+
+    double('Account', transaction_history: dummy_transaction)
+  end
+
+  let(:test_calc) do
+    double('Calculator', calculate_balance: 1000 )
+  end
+
   context 'when accessed' do
     it 'can print a properly formatted header' do
       expect{ test_printer.print_header }.to output("date || credit || debit || balance\n").to_stdout
     end
 
     it 'can print a transaction' do
-      expect{ test_printer.print_transaction }.to output("10-01-2012 || 1000.00 || || 1000.00 balance\n").to_stdout
+      p test_account.transaction_history[0]
+      p test_calc.calculate_balance
+      expect{ test_printer.print_transaction(test_account,0,test_calc) }.to output("10/01/2012 || 1000.00 ||  || 1000.00\n").to_stdout
     end
   end
 

ae6810d7c48ce44a8da9fa3646e7e57f121cab3d
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-31 08:45:28 +0000

    Fixed to accept new transaction object format

--- spec/balance_calc_spec.rb
+++ spec/balance_calc_spec.rb
@@ -1,8 +1,20 @@
 describe BalanceCalculator do
   let(:test_account) do
-    dummy_transaction = [{ amount: 1000, date: '10-01-2012' }, { amount: 2000, date: '13-01-2012' }, { amount: -500, date: '14-01-2012' }]
-    double('Account', transaction_history: dummy_transaction)
+    dummy_transactions = [test_transaction1, test_transaction2, test_transaction3]
+    
+    double('Account', transaction_history: dummy_transactions)
   end
+
+  let(:test_transaction1) do
+    double('Transaction', amount: 1000)
+  end
+  let(:test_transaction2) do
+    double('Transaction', amount: 2000)
+  end
+  let(:test_transaction3) do
+    double('Transaction', amount: -500)
+  end
+
   let(:test_calc) { BalanceCalculator.new }
 
   context 'when interacting with account' do
